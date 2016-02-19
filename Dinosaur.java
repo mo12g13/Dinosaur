5e84f6a92155bdbf41189524f60b5e6fd6bb2875
Sheveil Johnson <mo12g13@gmail.com>
2016-02-18 18:05:27 -0600

    I Started the Dinasour program.

---
+++ Dinosaur.java
@@ -0,0 +1,13 @@
+package com.Momo;
+
+public class Dinosaur {
+
+    public void roar() {
+        System.out.println("GRRRR!!!!");
+    }
+
+
+    public void eat() {
+        System.out.println("Crying");
+    }
+}
---
+++ DinosaurSuperVisor.java
@@ -0,0 +1,17 @@
+package com.Momo;
+
+/**
+ * Created by Sheveil Johnson on 2/18/2016.
+ */
+public class DinosaurSuperVisor {
+    public static void main(String[] args) {
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+        Dinosaur myDinosaur = new Dinosaur();
+        t_rex.roar();
+        velociraptor.roar();
+        velociraptor.eat();
+        t_rex.eat();
+        myDinosaur.eat();
+    }
+}
